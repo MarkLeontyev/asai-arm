31d68165a416c2f427082af565e465009c7acaf1
markleontyev <markleontyev123@gmail.com>
2025-09-03 00:33:03 +0200

    add manifest

---
+++ addons/asai_arm/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "АСАИ – Тестовое задание – АРМ",
+
+    'summary': "Автоматизированное рабочее место оператора",
+
+    'description': """
+Модуль для работы оператора: взять/выполнить/брак, учёт времени.
+    """,
+
+    'author': "Mark Leontev",
+    'website': "http://localhost:8069/asai/arm",
+
+    'category': 'Manufacturing',
+    'version': '18.0.1.0.0',
+
+    'depends': ['base', 'web', 'mail', 'uom'],
+
+    'data': [
+        'security/asai_arm_security.xml',
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/arm_task_views.xml',
+        'views/views.xml',
+        'views/templates.xml',
+        'data/arm_task_demo.xml',
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+}
+
